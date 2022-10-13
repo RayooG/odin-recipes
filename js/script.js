6f8799e30e343aa10b43802ccf80d9eba8b664cc
RayooG <petitdgithub@gmail.com>
2022-10-13 14:01:03 -0400

    page index.html modifie

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
+$(document).ready(function() {
+    $('#menu').click(function() {
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+
+    });
+
+    $('window').on('scroll load', function() {
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+    });
+
+});
+
+
+
+/*typing animation  */
 var typed = new Typed(".typing", {
-    strings: ["se koupe dwet", "se bon nan bouch", "san manti"],
+    strings: ["se koupe dwet", "se bon nan bouch", "Vant ou se Misyon n"],
     typeSpeed: 150,
     backSpeed: 150,
     loop: true
